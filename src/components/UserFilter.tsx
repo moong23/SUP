e9174fa3c2c058b9acfe8eaf7f2f070a2d69e626
moonki <moonki0623@naver.com>
2024-05-27 02:29:37 +0900

    details into one format

--- src/components/UserFilter.tsx
+++ src/components/UserFilter.tsx
@@ -84,18 +84,27 @@ const UserFilter = () => {
         })}
         <input
           type="text"
-          className="focus:outline-none text-[18px] text-[#a438f7a9]"
+          className="focus:outline-none text-[18px] text-[#a438f7a9] cursor-text"
           onKeyDown={handleKeyDown}
           tabIndex={1}
           placeholder="BOJ 아이디를 입력하세요"
           onBlur={handleBlur}
         />
       </span>
-      <span className="text-[#5B5B5B] font-medium text-base">
-        * 스터디원의 백준 아이디를 쉼표, 스페이스바, 엔터로 구분하여 넣어주세요{" "}
+      <details className="text-[#5B5B5B] font-medium text-base">
+        <summary>주의사항</summary>* 스터디원의 백준 아이디를 쉼표, 스페이스바,
+        엔터로 구분하여 넣어주세요 <br /> *{" "}
+        <a
+          href="https://solved.ac/"
+          target="_blank"
+          className="text-[#18CE3B]"
+        >
+          solved.ac
+        </a>
+        에 등록된 아이디에 한해서 검색이 가능합니다.
         <br /> * tab키를 누르거나, input이 blur될 때 문제를 검색합니다.
         <br />* 아이디를 클릭하면 삭제됩니다.
-      </span>
+      </details>
     </div>
   );
 };
